4119e0e69c5ff79ad18c679e521261c720121c28
sashomihajlov <sasho.mihajlov@gmail.com>
2022-10-14 09:59:44 +0200

    save image as original and thumbnail on uploads folder

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,42 @@
 import express from "express";
+import { registerMiddlewares } from "./middlewares/registerMiddlewares";
+import { fileStorageMiddleware } from "./middlewares/fileStorageMiddleware";
+import multer from "multer";
+import { fileStorage } from "./config/multer.config";
+import cors from "cors";
+import {
+  saveImage,
+  saveOriginalImage,
+  imageNameToLower,
+  saveThumbnailImage,
+} from "./utils/file.storage.helpers";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+
+// registerMiddlewares(app);
+// const upload = fileStorageMiddleware();
+const upload = multer();
 
 app.post("/graphql", (req, res) => {
   res.send("Implement GraphQL endpoint!");
 });
 
-app.post("/upload", (req, res) => {
-  res.send("Implement file upload endpoint!");
+app.post("/upload", upload.single("file"), async (req, res) => {
+  if (!req.file) {
+    res.status(401).json({ error: "Please provide an image" });
+  }
+  const imageName = imageNameToLower(req.file.originalname);
+  console.log("imageName", imageName);
+  try {
+    await saveOriginalImage(req.file.buffer, imageName);
+    await saveThumbnailImage(req.file.buffer, imageName);
+    res.status(200).json({ url: `/image/thumbnail/${imageName}`, name: imageName });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
